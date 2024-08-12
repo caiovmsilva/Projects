f6e3a237cef89cf533cdcdab6aba485f5243fbf5
Caio Vinícius <caiovmsilva123@gmail.com>
2024-08-11 22:24:51 -0300

    New feature - list
    Create list of cars

--- Web/Django/Django-Master/Projetos/carros/cars/views.py
+++ Web/Django/Django-Master/Projetos/carros/cars/views.py
@@ -5,12 +5,12 @@ from cars.models import Car
 #render sempre pede request, por padrão
 #esse request vem do usuário
 def cars_view(request):
-    Car.objects.all() #equivalente a Select * From Cars
+    cars = Car.objects.all() #equivalente a Select * From Cars
     
     return render(
         request, 
         'cars.html', 
         #dados sendo passadaos para o template
         #esta seria a lista dos carros mostradas para o usuário
-        {'cars': {'model': 'Astra 2.0'}}
+        {'cars': cars}
     )
