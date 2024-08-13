c98af874023cb2a095ca1de95d72d54aa8f5349b
Caio Vinícius <caiovmsilva123@gmail.com>
2024-08-13 12:24:38 -0300

    Update views - filter
    Stars using filter method.

--- Web/Django/Django-Master/Projetos/carros/cars/views.py
+++ Web/Django/Django-Master/Projetos/carros/cars/views.py
@@ -5,8 +5,11 @@ from cars.models import Car
 #render sempre pede request, por padrão
 #esse request vem do usuário
 def cars_view(request):
-    cars = Car.objects.all() #equivalente a Select * From Cars
-    
+    #cars = Car.objects.all() #equivalente a Select * From Cars
+    #busca qualquer model que contenha a string 'Chevette'
+    #Essa busca não é case sensitive, mas caracteres especiais devem estar presentes
+    cars = Car.objects.filter(model__contains='Chevette')
+
     return render(
         request, 
         'cars.html', 
